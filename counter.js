41ba3a9ab083c518722ea3a61329033c52ef238f
PSC Student <psc@billkuker.com>
2022-10-31 10:17:12 -0400

    Style.css changes + made cow and pig

--- counter.js
+++ counter.js
@@ -4,27 +4,27 @@
 
 //This code uses a FUNCTION named querySelector on the DOCUMENT
 //object to find two element, the button and the count.
-let button = document.querySelector("#button");
-let count = document.querySelector("#count");
+//let button = document.querySelector("#button");
+//let count = document.querySelector("#count");
 
 //A totally normal variable
-let counter = 0;
+//let counter = 0;
 
 //A totally normal function...
-function theyClickedTheButton() {
+//function theyClickedTheButton() {
   //It increments the counter variable. Remember that counter++
   //is shorthand for counter = counter + 1
-  counter++;
+  //counter++;
 
   //This is new, it sets the text inside the count element
   //to the value of the counter. This changes the number on
   //the web page!
-  count.innerText = counter;
-}
+ // count.innerText = counter;
+//}
 
 //This is new! It tells the button to CALL the "theyClickedTheButton"
 //function every time the 'click' event happens.
-button.addEventListener("click", theyClickedTheButton);
+//button.addEventListener("click", theyClickedTheButton);
 
 //So now that all of this has run the web browser is set up to
 //CALL that function every time the button is clicked.
